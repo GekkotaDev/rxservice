a3e74945f8133f0d7bfe712e7375bff6509e0222
gekkotadev <114091010+GekkotaDev@users.noreply.github.com>
2023-06-27 23:45:36 +0800

    Updated tests with Injector API

--- test/service_test.dart
+++ test/service_test.dart
@@ -2,7 +2,7 @@ import "package:test/test.dart";
 
 import "package:rxservice/rxservice.dart";
 
-class CounterService extends Service<int> {
+final class CounterService extends Service<int> {
   CounterService() : super(0);
 
   final memoSquare = computed<int, (int,)>();
@@ -14,28 +14,44 @@ class CounterService extends Service<int> {
     final (compute) = memoSquare;
     setState((count) => compute((count,), () => count * count));
   }
+
+  int mock() => 42;
+}
+
+final class MockCounterService extends CounterService {
+  @override
+  int mock() => 0;
 }
 
 void main() {
-  final counter = CounterService();
+  Injector.create(CounterService(), mock: MockCounterService());
 
   test("increments counter", () {
+    final counter = Injector.inject(() => CounterService());
     counter.increment(4);
     assert(counter.state == 4);
   });
 
   test("squares counter", () {
+    final counter = Injector.inject(() => CounterService());
     counter.square();
     assert(counter.state == 16);
   });
 
   test("decrements counter", () {
+    final counter = Injector.inject(() => CounterService());
     counter.decrement(6);
     assert(counter.state == 10);
   });
 
   test("resets counter", () {
+    final counter = Injector.inject(() => CounterService());
     counter.reset();
     assert(counter.state == 0);
+  });
+
+  test("counter has been mocked", () {
+    final counter = Injector.inject(() => CounterService());
+    assert(counter.mock() == 0);
   });
 }
